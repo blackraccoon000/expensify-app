3709ce7c91ef57292d53b472f825e2e803620ef9
Yutaka Fujii <whiteraccoon.breakable@gmail.comp>
2021-03-16 15:53:34 +0900

    調整

--- webpack.config.js
+++ webpack.config.js
@@ -8,7 +8,7 @@ module.exports = (env) => {
   return {
     entry: "./src/app.js",
     output: {
-      path: path.join(__dirname,"public"),
+      path: path.join(__dirname,"public","dist"),
       filename: "bundle.js"
     },
     module: {
@@ -47,6 +47,7 @@ module.exports = (env) => {
       contentBase: path.join(__dirname, 'public'),
       historyApiFallback: true,
       port: 8081,
+      publicPath: "/dist/",
     }
   }
 }
