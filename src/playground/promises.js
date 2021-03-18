541bba82fd6034f40a9de7b32d9bc1d3be2e388c
Yutaka Fujii <whiteraccoon.breakable@gmail.comp>
2021-03-18 16:35:53 +0900

    promiseについて学び、firebaseから情報を取得する方法を学習した

---
+++ src/playground/promises.js
@@ -0,0 +1,18 @@
+// const promise = new Promise((resolve, reject)=>{
+//   setTimeout(() => {
+//     // resolve({
+//     //   name:"Rac",
+//     //   age:34
+//     // })
+//     reject("something went wrong")
+//   }, 1500);
+// })
+
+// console.log("before")
+
+// promise.then(data=>console.log("1",data))
+// .catch(error=>console.log("error:",error))
+
+// promise.then(data=>console.log("1",data),error=>console.log("error:",error))
+
+// console.log("after")
