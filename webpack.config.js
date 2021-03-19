70460942e896bc9bec320691690af63de72fedcc
Yutaka Fujii <whiteraccoon.breakable@gmail.comp>
2021-03-19 15:12:32 +0900

    tiny fix

--- webpack.config.js
+++ webpack.config.js
@@ -4,6 +4,8 @@ const ExtractTextPlugin = require("extract-text-webpack-plugin");
 
 process.env.NODE_ENV = process.env.NODE_ENV || "development"
 
+console.log("ppppppppppp",process.env.NODE_ENV)
+
 if(process.env.NODE_ENV === "test") {
   require("dotenv").config({ path: ".env.test" })
 } else if(process.env.NODE_ENV === "development") {
